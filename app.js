557a8e1ac17dab6463a2c34525ddb095a424bd71
Mary Kaki <justonly4testing@gmail.com>
2021-03-14 09:19:57 +0000

    changed app.js and list.ejs

--- app.js
+++ app.js
@@ -13,34 +13,18 @@ app.get("/", function(req, res){
   var currentDay = today.getDay();
   var day = "";
 
-  switch(currentDay){
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
 
-    case 0:
-    day = "Sunday";
-    break;
-    case 1:
-    day = "Monday";
-    break;
-    case 2:
-    day = "Tuesday";
-    break;
-    case 3:
-    day = "Wednesday";
-    break;
-    case 4:
-    day = "Thursday";
-    break;
-    case 5:
-    day = "Friday";
-    break;
-    case 6:
-    day = "Saturday";
-    break;
-    default:
-    console.log("There is not 8th day!");
-  }
+var day = today.toLocaleString("en-US", options);
 
-  res.render("list", {kindOfDay:day});
+
+  res.render("list",
+  {kindOfDay: day
+  });
 
 });
 
