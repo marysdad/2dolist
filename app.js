230b25ffb76403a1b5fee6a9d03f171a4c0785d8
Mary Kaki <justonly4testing@gmail.com>
2021-03-14 10:17:40 +0000

    changed so the list working at a basic level

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
@@ -22,11 +26,22 @@ var options = {
 var day = today.toLocaleString("en-US", options);
 
 
-  res.render("list",
-  {kindOfDay: day
-  });
+  res.render("list",{kindOfDay: day, newListItems: items});
 
 });
+
+app.post("/", function(req, res){
+   var item = req.body.newItem;
+
+   items.push(item);
+
+  res.redirect("/");
+})
+
+
+
+
+
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
