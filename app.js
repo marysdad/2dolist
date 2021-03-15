f2c65808b7d107ff5ba50648d812263aaa9ed423
Mary Kaki <justonly4testing@gmail.com>
2021-03-15 13:05:06 +0000

    I have finished  the ToDoList on the 15th March 2021; I learnt to separated some js code into a module and import it to use it in the main code file i.e. app.js. I had to do a lot of debugging but it was worth the effort.

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
-//jshint eversion
+//jshint eversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-var items = ["Buy Food","Cook Food","Eat Food","Say alhamdulillah"];
-
+let items = ["Buy Food", "Cook Food", "Eat Food", "Say alhamdulillah"];
+let workItems = [];
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+let day = date.getDate();
 
-var options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long"
-};
-
-var day = today.toLocaleString("en-US", options);
-
-
-  res.render("list",{kindOfDay: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
-app.post("/", function(req, res){
-   var item = req.body.newItem;
+app.post("/", function(req, res) {
 
-   items.push(item);
 
-  res.redirect("/");
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+    console.log("hello")
+
+  } else {
+    items.push(item);
+    res.redirect("/");
+    console.log("hi")
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List",  newListItems: workItems});
+});
+
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
 })
 
-
-
-
-
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server started on port 3000");
 });
